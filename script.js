47c99225dfaf95d6e9b2ffb1c2a432cc7159f0d9
Samuel Horry <211251918+Sam-Horry@users.noreply.github.com>
2025-10-08 17:39:21 +0100

    keyboard navigation
    added my first bit of javascript to allow for the visitor to use their
    keyboard to navigate the different pages of the site, by pressing the
    number that corresponds to the page shown in the navigation panel

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('keydown', function (event) {
+  switch (event.key) {
+    case '1':
+      window.location.href = "index.html";
+      break;
+    case '2':
+      window.location.href = "projects.html";
+      break;
+    case '3':
+      window.location.href = "skills.html";
+      break;
+    case '4':
+      window.location.href = "contact.html";
+      break;
+  }
+});
